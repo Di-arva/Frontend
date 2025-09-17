cc8accac40fb41f89f5115e464a4bc9e1b6d2437
Nishi Surti <01nishisurti@gmail.com>
2025-09-16 20:32:53 -0400

    hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,29 +1,40 @@
+import StickyLabel from "./StickyLabel";
+
 const Hero = () => {
   return (
     <div className="mb-20">
-      <div className="w-full bg-darkblue h-10 opacity-85 flex items-center fixed font-poppins text-md font-normal  top-0  ">
-        <ul className=" flex gap-4 text-lightbg mx-4  w-full justify-around ">
-          <li>We provide support in English</li>
-          <li>ਅਸੀਂ ਪੰਜਾਬੀ ਵਿੱਚ ਸਹਾਇਤਾ ਪ੍ਰਦਾਨ ਕਰਦੇ ਹਾਂ ।</li>
-          <li>zapewniamy wsparcie w języku polskim</li>
-          <li>हम हिन्दी में सहायता प्रदान करते हैं।</li>
-          <li>અમે ગુજરાતીમાં મદદ કરીએ છીએ.</li>
-        </ul>
-      </div>
-
-      <div className="flex mt-10 ml-10">
-        <div className="flex flex-col   justify-center">
+      <StickyLabel />
+      <div className="flex ml-55">
+        <div className="flex flex-col z-10 justify-center h-screen ">
           <h1 className="text-9xl font-bold text-darkblue">
-            Support that <br /> you can trust
+            Support that you can trust
           </h1>
-          <p className="font-poppins w-148  ml-2 mt-10 text-darkblack">
+          <p className="font-poppins w-200 mt-10 text-darkblack">
             We comprehensive health solutions tailored to your needs. With
             advanced medical practices, modern technology, and a patient first
             approach, we ensure high-quality care at accessible costs supporting
             your well-being at every stage of life.
           </p>
+          <div className="flex mt-8 gap-8">
+            <button class="bg-white text-sm  font-poppins text-darkblue border border-darkblue px-6 py-3 font-semibold rounded-full  hover:cursor-pointer">
+              Create Account
+            </button>
+            <button class="bg-darkblue text-lightbg px-6 py-3 rounded-full font-semibold hover:cursor-pointer">
+              Talk to us
+            </button>
+          </div>
         </div>
-        <div className="flex-1 w-full border z-10  border-red-500 bg-lightblue pl-180"></div>
+        <div className="flex w-full -ml-90 bg-lightblue">
+          <div className="text-[650px] -mr-30 font-bold  items-center  text-darkblue opacity-20 ">
+            •
+          </div>
+          <div className="text-[750px] font-bold justify-center items-center flex text-darkblue opacity-20 ml-30">
+            D
+          </div>
+          <div className="text-[650px] font-bold  items-center  text-darkblue opacity-20 ">
+            •
+          </div>
+        </div>
       </div>
     </div>
   );
